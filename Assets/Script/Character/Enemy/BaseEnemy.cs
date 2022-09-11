7d50c75e33d5c35f68fd0cda3c9ded84d3fe1771
LAPTOP-CI3C75GC\Atlplle <2276021874@qq.com>
2022-09-11 22:26:51 +0800

    移除了状态机模式

--- Assets/Script/Character/Enemy/BaseEnemy.cs
+++ Assets/Script/Character/Enemy/BaseEnemy.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class BaseEnemy : MonoBehaviour
 {
     public E_EnemyType enemyType;
-    [DisplayOnly] public E_EnemyState currentState;
-
-
-    private BaseState baseState;
-    public PatrolState patrolState = new PatrolState();
-    public GuardState guardState = new GuardState();
-    public AttackState attackState = new AttackState();
-    public ChaseState chaseState = new ChaseState();
-    public FlyState flyState = new FlyState();
 
     protected Animator anim;
     protected Rigidbody2D rb;
@@ -35,53 +26,18 @@ public class BaseEnemy : MonoBehaviour
 
     protected virtual void Start()
     {
-        StartState();
+
     }
 
     protected virtual void Update()
     {
-        if (baseState != null)
-            baseState.OnUpdate(this);
+
     }
 
     protected virtual void FixedUpdate()
     {
-        if (baseState != null)
-            baseState.OnFixedUpdate(this);
+
     }
-
-    public void SwitchState(BaseState state)
-    {
-        if (baseState != null)
-        {
-            baseState.OnExit(this);
-        }
-
-        baseState = state;
-        baseState.OnStart(this);
-    }
-
-    private void StartState()
-    {
-        switch (enemyType)
-        {
-            case E_EnemyType.Ground:
-                SwitchState(patrolState);
-                break;
-            case E_EnemyType.Fly:
-                SwitchState(flyState);
-                break;
-            case E_EnemyType.Static:
-                SwitchState(guardState);
-                break;
-            case E_EnemyType.Boss:
-                break;
-            case E_EnemyType.Guard:
-                SwitchState(guardState);
-                break;
-        }
-    }
-
 
     protected void GroundMovement(bool isRight, float moveSpeed)
     {
